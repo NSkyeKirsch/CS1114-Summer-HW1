6cc04054c8953f6db226a754768be280f0ed3b48
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-05-30 17:10:46 -0400

    finished q1

---
+++ hw1_q1.py
@@ -0,0 +1,13 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW1 - Q1
+Date due: 2022-05-31
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+name = input("Please enter your name: ")
+print("Hi", name + "!", "Welcome to CS-UY 1114!")
+
